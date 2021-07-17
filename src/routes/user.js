52dc03525e1448afbd6fa8b25a3970c243b9f052
stephannils <steniljeh@gmail.com>
2021-07-17 09:57:00 +0200

    implemented, login/logout, edit/delete profile

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 
 const router = new express.Router();
 
@@ -18,10 +19,81 @@ router.post('/users', async (req, res) => {
   const user = new User(req.body);
 
   try {
+    const token = await user.generateAuthToken();
     await user.save();
-    res.status(201).send(user);
+    res.status(201).send({user, token});
   } catch (err) {
-    res.status(400).send(err);
+    res.status(400).send();
+  }
+});
+
+// Login user
+
+/* 
+{
+  "email": "stephansemail@fantasyproviderofchoice.com",
+  "password": "Password123" 
+}
+*/
+
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCreds(req.body.email, req.body.password);
+    const token = await user.generateAuthToken();
+    res.send({user, token});
+  } catch (err) {
+    res.status(400).send();
+  }
+});
+
+// Show profile
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user);
+});
+
+// Update profile
+/* 
+  "name": "Stephan",
+  "email": "stephansemail@fantasyproviderofchoice.com",
+  "password": "Password123"
+*/
+router.post('/users/me', auth, async (req, res) => {
+  const allowedUpdates = ['email', 'password', 'name'];
+  const updates = Object.keys(req.body);
+  const isValid = updates.every((update) => allowedUpdates.includes(update));
+
+
+  if (!isValid) {
+    res.status(400).send({error: 'Invalid updates'});
+  }
+
+  try {
+    updates.forEach(update => req.user[update] = req.body[update]);
+    await req.user.save();
+    res.send(req.user);
+  } catch (err) {
+    res.status(400).send();
+  }
+});
+
+// Delete user
+router.get('/users/me/deleteUser', auth, async (req, res) => {
+  try {
+    await req.user.remove();
+    res.send(req.user);
+  } catch (err) {
+    res.status(500).send();
+  }
+});
+
+// Logout
+router.get('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (err) {
+    req.status(500).send();
   }
 });
 
