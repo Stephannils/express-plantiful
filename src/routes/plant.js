071a4e76e6e9c5adfe20bad01ac947da7d671545
stephannils <steniljeh@gmail.com>
2021-07-20 14:15:09 +0200

    added delete plant by id

--- src/routes/plant.js
+++ src/routes/plant.js
@@ -4,7 +4,7 @@ const auth = require ('../middleware/auth');
 
 const router = new express.Router();
 
-
+// Create plant
 router.post('/plants', auth, async (req, res) => {
   const plant = new Plant({
     ...req.body,
@@ -16,6 +16,21 @@ router.post('/plants', auth, async (req, res) => {
     res.status(201).send(plant);
   } catch (err) {
     res.status(400).send();
+  }
+});
+
+// Delete plant
+router.get('/plants/:id', auth, async (req, res) => {
+  try {
+    const task = await Plant.findOneAndDelete({_id: req.params.id, owner: req.user._id});
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (err) {
+    res.status(500).send();
   }
 });
 
