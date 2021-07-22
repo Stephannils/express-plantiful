16de2ac3aeb17bd3b177f4f76cdb93da052499da
stephannils <steniljeh@gmail.com>
2021-07-22 14:12:20 +0200

    finished delete plants collection upon delete user

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Plant = require('./plant');
 
 const userSchema = mongoose.Schema(
   {
@@ -87,6 +88,14 @@ userSchema.pre('save', async function(next) {
   if (user.isModified('password')) {
     user.password = await bcrypt.hash(user.password, 8);
   }
+
+  next();
+});
+
+userSchema.pre('remove', async function(next) {
+  const user = this;
+
+  await Plant.deleteMany({owner: user._id});
 
   next();
 });
